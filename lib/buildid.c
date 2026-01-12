abc867c00868f03e7641bd285186a0848f8f40c1
Patrick Roy <patrick.roy@linux.dev>
2026-01-12 15:29:36 +0000

    mm: introduce AS_NO_DIRECT_MAP
    Add AS_NO_DIRECT_MAP for mappings where direct map entries of folios are
    set to not present. Currently, mappings that match this description are
    secretmem mappings (memfd_secret()). Later, some guest_memfd
    configurations will also fall into this category.
    
    Reject this new type of mappings in all locations that currently reject
    secretmem mappings, on the assumption that if secretmem mappings are
    rejected somewhere, it is precisely because of an inability to deal with
    folios without direct map entries, and then make memfd_secret() use
    AS_NO_DIRECT_MAP on its address_space to drop its special
    vma_is_secretmem()/secretmem_mapping() checks.
    
    Use a new flag instead of overloading AS_INACCESSIBLE (which is already
    set by guest_memfd) because not all guest_memfd mappings will end up
    being direct map removed (e.g. in pKVM setups, parts of guest_memfd that
    can be mapped to userspace should also be GUP-able, and generally not
    have restrictions on who can access it).
    
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Acked-by: David Hildenbrand (Red Hat) <david@kernel.org>
    Signed-off-by: Patrick Roy <patrick.roy@linux.dev>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Nikita Kalyazin <nikita.kalyazin@linux.dev>

--- lib/buildid.c
+++ lib/buildid.c
@@ -65,10 +65,11 @@ static int freader_get_folio(struct freader *r, loff_t file_off)
 
 	freader_put_folio(r);
 
-	/* reject secretmem folios created with memfd_secret() */
-	if (secretmem_mapping(r->file->f_mapping))
+	/* reject folios without direct map entries (e.g. from memfd_secret() or guest_memfd()) */
+	if (mapping_no_direct_map(r->file->f_mapping))
 		return -EFAULT;
 
+	/* only use page cache lookup - fail if not already cached */
 	r->folio = filemap_get_folio(r->file->f_mapping, file_off >> PAGE_SHIFT);
 
 	/* if sleeping is allowed, wait for the page, if necessary */
@@ -114,6 +115,12 @@ static const void *freader_fetch(struct freader *r, loff_t file_off, size_t sz)
 			return NULL;
 		}
 		return r->data + file_off;
+	}
+
+	/* reject folios without direct map entries (e.g. from memfd_secret() or guest_memfd()) */
+	if (mapping_no_direct_map(r->file->f_mapping)) {
+		r->err = -EFAULT;
+		return NULL;
 	}
 
 	/* fetch or reuse folio for given file offset */
